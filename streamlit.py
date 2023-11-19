66eac525114399985ce7efe1056e90e8a38a4047
guptasainikhil <69230792+guptasainikhil@users.noreply.github.com>
2023-11-19 02:19:16 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,35 +2,28 @@ import streamlit as st
 import replicate
 import os
 
+# Initialize session state for messages
 if 'messages' not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Welcome to the DMV chatbot. How may I assist you today?"}]
 
-# App title
 st.set_page_config(page_title="🚗 DMV Llama 2 Chatbot")
 st.title('🚗 DMV Llama 2 Chatbot')
 
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-# Replicate Credentials
+# Replicate Credentials and settings
 with st.sidebar:
-    st.title('🚗 DMV Llama 2 Chatbot')
+    st.title('Settings')
     st.write('This DMV chatbot is powered by the Llama 2 LLM model from Meta.')
-    
+
     # API token input
     if 'REPLICATE_API_TOKEN' in st.secrets:
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
         st.success('API key already provided!', icon='✅')
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if replicate_api and not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-            st.warning('Please enter valid credentials!', icon='⚠️')
-        elif replicate_api:
+        if replicate_api:
+            os.environ['REPLICATE_API_TOKEN'] = replicate_api
             st.success('Credentials accepted. You can now use the chatbot.', icon='👍')
     
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
-
     # Model and parameters selection
     st.subheader('Models and parameters')
     selected_model = st.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
@@ -39,18 +32,9 @@ with st.sidebar:
     max_length = st.slider('Max Length', 32, 512, 120, 8)
     st.markdown('📖 Learn more about this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
-# Store LLM generated responses
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "Welcome to the DMV chatbot. How may I assist you today?"}]
-
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "Chat history cleared. How may I assist you?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+    # Button to clear chat history
+    if st.button('Clear Chat History'):
+        st.session_state.messages = [{"role": "assistant", "content": "Chat history cleared. How may I assist you?"}]
 
 # Function for generating LLaMA2 response
 def generate_llama2_response(prompt_input):
@@ -58,20 +42,21 @@ def generate_llama2_response(prompt_input):
     for dict_message in st.session_state.messages:
         string_dialogue += f"{dict_message['role'].capitalize()}: {dict_message['content']}\n\n"
     
-    model_endpoint = 'a16z-infra/llama7b-v2-chat:...' if selected_model == 'Llama2-7B' else 'a16z-infra/llama13b-v2-chat:...'
+    # Replace with actual model endpoint
+    model_endpoint = 'a16z-infra/llama7b-v2-chat:actual_endpoint' if selected_model == 'Llama2-7B' else 'a16z-infra/llama13b-v2-chat:actual_endpoint'
     output = replicate.run(model_endpoint, 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1})
     return output
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
+if prompt := st.text_input('Ask a question:'):
     st.session_state.messages.append({"role": "user", "content": prompt})
     response = generate_llama2_response(prompt)
     response_text = ''.join(response) if response else "Sorry, I couldn't generate a response."
     st.session_state.messages.append({"role": "assistant", "content": response_text})
 
 # Display the chat
-for message in st.session_state.messages[-2:]:  # Display the last user message and the generated response
+for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
